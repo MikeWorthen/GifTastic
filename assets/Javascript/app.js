cfc8d7a6118d26a63d2191202b116d6334eb379b
Mike Worthen <mikeworthen55@gmail.com>
2019-06-18 12:49:45 -0600

    On click is working

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -3,8 +3,10 @@ $("button").on("click", function() {
 
     var sports = $(this).attr("data-person");
     
+    //API url
     var url =  "https://api.giphy.com/v1/gifs/search?q=" +
     sports + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+
 
     $.ajax({
         url: url,
@@ -15,13 +17,10 @@ $("button").on("click", function() {
         for (var i = 0; i < results.length; i++) {
             var gifDiv = $("<div>");
             var gifImage = $("<img>");
-            gifImage.attr("src", results[i].images.url);
+            gifImage.attr("src", results[i].images.fixed_height.url);
             gifDiv.append(gifImage);
             $("#gifList").prepend(gifDiv);
-
         }
-
       });
-    
     });
 
