cb4413e06b654367e1ebc9bb727031a64a5b8985
Mike Worthen <mikeworthen55@gmail.com>
2019-06-17 23:34:35 -0600

    Getting the form built

---
+++ assets/Javascript/app.js
@@ -0,0 +1,27 @@
+
+$("button").on("click", function() {
+
+    var sports = $(this).attr("data-person");
+    
+    var url =  "https://api.giphy.com/v1/gifs/search?q=" +
+    sports + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+
+    $.ajax({
+        url: url,
+        method: "GET"
+      })
+      .then(function(response) {
+        var results = response.data;
+        for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div>");
+            var gifImage = $("<img>");
+            gifImage.attr("src", results[i].images.url);
+            gifDiv.append(gifImage);
+            $("#gifList").prepend(gifDiv);
+
+        }
+
+      });
+    
+    });
+
